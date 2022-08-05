5ec4c95ce649aeb2135c158cdb9ef4d2cc74938d
Oliver Wilkes <91676214+Oliver-Wilkes@users.noreply.github.com>
2022-08-05 12:52:06 +0100

    Addition of Bootstrap Navbar and slight Maps Window styling

--- script.ts
+++ script.ts
@@ -1,16 +1,15 @@
-
         // Initialize and add the map
-function initMap(): void {
-  // The location of Uluru
-  const uluru = { lat: -25.344, lng: 131.031 };
-  // The map, centered at Uluru
-  const map = new google.maps.Map(
-    document.getElementById("map") as HTMLElement,
-    {
-      zoom: 4,
-      center: uluru,
-    }
-  );
+        function initMap(): void {
+            // The location of Uluru
+            const uluru = { lat: -25.344, lng: 131.031 };
+            // The map, centered at Uluru
+            const map = new google.maps.Map(
+              document.getElementById("map") as HTMLElement,
+              {
+                zoom: 4,
+                center: uluru,
+              }
+            );
 
   // The marker, positioned at Uluru
   const marker = new google.maps.Marker({
@@ -28,11 +27,5 @@ window.initMap = initMap;
 
 function initMap() {}
 
-// The location of Uluru
-const uluru = { lat: -25.344, lng: 131.031 };
-// The map, centered at Uluru
-const map = new google.maps.Map(document.getElementById("map"), {
-  zoom: 4,
-  center: uluru,
-});
+
     
