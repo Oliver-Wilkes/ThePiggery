4423bb3bdfd998e0af52e04cbb30db4aad97f8e8
Oliver Wilkes <91676214+Oliver-Wilkes@users.noreply.github.com>
2022-08-10 11:23:30 +0100

    Fixing maps API and setting it to satellite view by default

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-//  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCs0eoO8UcMZKu8iqRhgJP3-XhTIUdGEVA&callback=initMap" ;
-
 // The location of The Piggery
 const Piggery = { lat: 51.191490, lng: -2.636926 };
 // The map, centered at The Piggery
 const map = new google.maps.Map(document.getElementById("map"), {
-  zoom: 16,
+  zoom: 18,
   center: Piggery,
+  mapTypeId: 'satellite'
 }); 
 
 
   const marker = new google.maps.Marker({
     position: Piggery,
     map: map,
-
+    
   });
-
- 
-
-
- 
-  // type="text/javascript";
